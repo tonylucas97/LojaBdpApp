eea5bd9d8c71c7d4c8c304ea4793772a199f2137
Tony Silva <tonysilva@MacBook-Air-de-Tony.local>
2021-07-02 11:42:51 -0300

    asdasdasd

--- src/LogedNavigation/LogedNavigation.js
+++ src/LogedNavigation/LogedNavigation.js
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Catalogo from "../Catalogo/Catalogo";
 import Carrinho from "../Carrinho/Carrinho";
 import Configuracoes from "../Configuracoes/Configuracoes";
 import { Text, View } from "react-native"
 
-
+const Tab = createBottomTabNavigator();
 
 export default function LogedNavigation() {
     return (
-        <Text>asdasdas</Text>
+        <Tab.Navigator tabBarOptions={{showLabel:false,style:{position:"absolute",bottom:15,elevation:0,backgroundColor:"#F7F9FA"}}}>
+            <Tab.Screen name="Catalogo" component={Catalogo}/>
+            <Tab.Screen name="Carrinho" component={Carrinho}/>
+            <Tab.Screen name="Configuracoes" component={Configuracoes}/>
+        </Tab.Navigator>
     )
 }
